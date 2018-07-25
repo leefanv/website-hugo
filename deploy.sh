314dd56d217e6f34ebec6e6e51f32e5f1c8ea8fb
leefanv <42181296@qq.com>
2018-07-25 16:21:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git push origin master
 
 
 # Build the project. 
-hugo nederburg # if using a theme, replace by `hugo -t <yourtheme>`
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
 # Go To Public folder
 cd public
